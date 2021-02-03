1d7d046342bbe0585d199f7cd12cb522cdef4c03
khanhvu207 <k2.vu@student.vu.nl>
2021-02-03 01:04:37 +0100

    add: biTemperedLoss

--- libs/losses/__init__.py
+++ libs/losses/__init__.py
@@ -1,4 +1,5 @@
 from .classification.crossentropy import BCEWithLogitsLoss, WeightedBCEWithLogitsLoss, CrossEntropyLoss
 from .classification.focalloss import FocalLoss, FocalCosineLoss
+from .classification.bi_tempered_loss import BiTemperedLoss
 from .segmentation.diceloss import DiceLoss
 from .mixedloss import MixedLoss
