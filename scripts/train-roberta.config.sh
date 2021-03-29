9fe8ef2592cf91c083be7bafbefbaa63796c5373
Taewoon Kim <tae898@gmail.com>
2021-03-29 15:23:44 +0200

    Make cleaning utterances modular

--- scripts/train-roberta.config.sh
+++ scripts/train-roberta.config.sh
@@ -1,4 +1,4 @@
-DATASET="EmoryNLP"
+DATASET="DailyDialog"
 
 if [ "${DATASET}" = MELD ]; then
     NUM_CLASSES=7
@@ -13,10 +13,11 @@ else
     exit 1
 fi
 
+CLEAN_UTTERANCES=true                                  # clean utterances (true or false)
 METRIC=cross_entropy_loss                              # should be one of f1_weighted, f1_micro, f1_macro, or cross_entropy_loss
-SPEAKER_MODE=upper                                      # should be one of title, upper, lower, none
+SPEAKER_MODE=upper                                     # should be one of title, upper, lower, none
 SEEDS=0,1,2,3,4                                        # random seeds
-NUM_UTTS=1                                             # number of utterances in one sequence
+NUM_UTTS=10                                            # number of utterances in one sequence
 WORKERS=60                                             # number of workers for preprocessing RoBERTa
 LR=1e-05                                               # Peak LR for polynomial LR scheduler.
 BATCH_SIZE=4                                           # Batch size, per GPU
