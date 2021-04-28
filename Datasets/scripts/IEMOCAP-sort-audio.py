85cdf1e261e826445a93571cd9dbd5c9b17b3846
Taewoon Kim <tae898@gmail.com>
2021-04-28 14:08:55 +0200

    Add submodule

--- Datasets/scripts/IEMOCAP-sort-audio.py
+++
@@ -1,9 +0,0 @@
-import os
-from glob import glob
-
-os.chdir('IEMOCAP/raw-audios')
-    
-for filename in glob('./*.wav'):
-    DIR = '_'.join(filename.split('_')[:-1])
-    os.makedirs(DIR, exist_ok=True)
-    os.rename(filename, os.path.join(DIR, filename))
